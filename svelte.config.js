4f8c8762aecd9b68668dc46e79753ba646f51d2b
ylor <ylor@git>
2023-01-02 23:02:48 -0500

    test: it's working?

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import adapter from '@sveltejs/adapter-auto';
-import vercel from '@sveltejs/adapter-vercel';
+// import vercel from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 const config = {
