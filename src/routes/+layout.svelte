736d3902326664720a2c30d249165387bbaa3a6d
ylor <ylor@git>
2023-01-06 17:13:53 -0500

    tweak: style for mobile

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,6 @@
 <script>
 	import '@fontsource/jetbrains-mono/variable.css';
 	import '../app.css';
-	// import '$lib/assets/fonts/JetBrainsMono/font.css';
 </script>
 
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,9 +13,9 @@
 	style="background-image: url({data.gif})"
 />
 
-<main class="flex h-screen flex-col justify-center p-4">
+<main class="flex h-full flex-col justify-center p-2 md:h-screen">
 	<div
-		class="flex h-screen w-full overflow-hidden rounded-2xl border border-neutral-800/25 bg-black/50 shadow-xl backdrop-blur-sm md:mx-auto md:max-h-[600px] md:max-w-screen-lg md:bg-neutral-900/50"
+		class="flex h-screen w-full overflow-hidden rounded-2xl border border-neutral-600/25 bg-black/40 drop-shadow-xl backdrop-blur backdrop-saturate-150 md:mx-auto md:max-h-[600px] md:max-w-screen-lg md:bg-neutral-900/50"
 	>
 		<img class="hidden aspect-square object-cover md:block md:w-2/5" src={data.gif} alt="" />
 		{#if $searching}
