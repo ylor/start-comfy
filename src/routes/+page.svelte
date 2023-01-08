5621700cd36ba7d5f78b19f5ada89f94e3f82575
ylor <ylor@git>
2023-01-08 16:59:14 -0500

    style:crt filter only on hero image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,7 +9,7 @@
 </script>
 
 <div
-	class="fixed inset-0 -z-10 h-full w-full bg-cover bg-center md:hidden"
+	class="crt fixed inset-0 -z-10 h-full w-full bg-cover bg-center md:hidden"
 	style="background-image: url({data.gif})"
 />
 
@@ -17,7 +17,13 @@
 	<main
 		class="flex w-full grow overflow-hidden rounded-xl border border-neutral-700/25 bg-black/50 shadow backdrop-blur-sm backdrop-saturate-150 md:mx-auto md:max-h-[600px] md:max-w-screen-lg md:bg-neutral-900/50"
 	>
-		<img class="hidden aspect-square object-cover md:block md:w-2/5" src={data.gif} alt="" />
+		<picture class="crt -z-10 md:w-2/5">
+			<img
+				class="hidden aspect-square h-full w-full object-cover md:block "
+				src={data.gif}
+				alt=""
+			/>
+		</picture>
 		<section class="relative mt-4 w-full md:w-3/5">
 			{#if $searching}
 				<Search />
