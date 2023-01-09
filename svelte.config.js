9b6ac1a021826f8c57bafdc482ae5ef14ae73414
ylor <ylor@git>
2023-01-08 23:16:14 -0500

    test: use edge server

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
-import adapter from '@sveltejs/adapter-auto';
-// import vercel from '@sveltejs/adapter-vercel';
+//import adapter from '@sveltejs/adapter-auto';
+import vercel from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 const config = {
 	kit: {
-		adapter: adapter()
-		// adapter: vercel({ edge: true })
+		//adapter: adapter()
+		adapter: vercel({ edge: true })
 	},
 	preprocess: vitePreprocess({ postcss: true })
 };
